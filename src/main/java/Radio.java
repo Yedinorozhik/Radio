c9453fad2c2c3e357e95d86723311397d8cddda1
yedinorozhik <misterrpandaa@gmail.com>
2025-03-31 12:28:08 +0300

    Initial commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,44 @@
+public class Radio {
+
+    public int currentVolume;
+    public int currentStation;
+
+
+    public void increaseVolume(int newCurrentVolume) {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void setToMaxVolume() {
+        currentVolume = 100;
+    }
+
+    public void setToMinVolume() {
+        currentVolume = 0;
+    }
+
+    public void totalNumberStation(int newCurrentStation) {
+        if (currentStation >= 0 && currentStation <= 9) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = 0;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void next() {
+        if (currentStation > 9) {
+            currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation < 0) {
+            currentStation = 9;
+        }
+    }
+
+
+    }
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void maxVolume() {
+        Radio radio = new Radio();
+
+        radio.setToMaxVolume();
+
+        int expected = 100;
+        int actual = radio.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minVolume() {
+        Radio radio = new Radio();
+
+        radio.setToMinVolume();
+
+        int expected = 0;
+        int actual = radio.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void MaxStation() {
+        Radio radio = new Radio();
+
+        radio.next();
+
+        int expected = 0;
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+
+}
